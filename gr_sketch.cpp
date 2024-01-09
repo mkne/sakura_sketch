fa8e52591c06ea4fceb51e493dfd68f4888a1644
Marc Kreidler <kreidler@nedo.com>
2024-01-09 15:56:06 +0100

    First experiments with stepper motor over DRV8833 on Pins 6,7,8,9
    upper limit switch (XO) on Pin 1
    lower limit switch (XU) on Pin 0

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -1,33 +1,50 @@
 /* GR-SAKURA Sketch Template V2.24 */
 #include <Arduino.h>
-
-#define INTERVAL 50
+#include <Stepper.h>
+int SPU = 2048;
+Stepper Motor(SPU, 6,7,8,9);
+int XU=0; // Endschalter unten an Pin0
+int XU_reached=0; // 1=>Endposition X unten erreicht
+int XO=1; // Endschalter oben an Pin1 
+int XO_reached=0; // 1=>Endposition X oben erreicht
+int Tasterstatus=0; // Variable für den Status des Tasters
+int direction=1; // -1=>down 1=>up
 
 void setup()
 {
+    Motor.setSpeed(4);
+    pinMode(XU, INPUT);
+    pinMode(XO, INPUT);
     pinMode(PIN_LED0,OUTPUT);
     pinMode(PIN_LED1,OUTPUT);
     pinMode(PIN_LED2,OUTPUT);
     pinMode(PIN_LED3,OUTPUT);
-
 }
 
-void loop()
+void loop() 
 {
-    digitalWrite(PIN_LED0, 1);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED1, 1);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED2, 1);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED3, 1);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED0, 0);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED1, 0);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED2, 0);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED3, 0);
-    delay(INTERVAL);
+    XU_reached=!digitalRead(XU);
+    
+    if(direction==-1 & !XU_reached)
+    {
+		digitalWrite(PIN_LED0, 0);
+        Motor.step(-1); //...Drehe den Motor um einen einzigen Schritt.
+    }
+    else
+    {
+        digitalWrite(PIN_LED0, 1);
+        direction=1;
+    }
+    
+    XO_reached=!digitalRead(XO);
+    if(direction==1 & !XO_reached)
+    {
+        digitalWrite(PIN_LED3, 0);
+        Motor.step(1); //...Drehe den Motor um einen einzigen Schritt.
+    }
+    else
+    {
+		digitalWrite(PIN_LED3, 1);
+		direction=-1; 
+    }
 }
