61d470a9169bd1f467f304e923f9fbf6dc787f45
Marc Kreidler <kreidler@nedo.com>
2024-01-09 15:40:45 +0100

    Lauflicht mit Onboard LEDs im Nightrider-style

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -1,7 +1,7 @@
 /* GR-SAKURA Sketch Template V2.24 */
 #include <Arduino.h>
 
-#define INTERVAL 50
+#define INTERVAL 150
 
 void setup()
 {
@@ -16,18 +16,20 @@ void loop()
 {
     digitalWrite(PIN_LED0, 1);
     delay(INTERVAL);
+    digitalWrite(PIN_LED0, 0);
     digitalWrite(PIN_LED1, 1);
     delay(INTERVAL);
+    digitalWrite(PIN_LED1, 0);
     digitalWrite(PIN_LED2, 1);
     delay(INTERVAL);
+    digitalWrite(PIN_LED2, 0);
     digitalWrite(PIN_LED3, 1);
     delay(INTERVAL);
-    digitalWrite(PIN_LED0, 0);
-    delay(INTERVAL);
-    digitalWrite(PIN_LED1, 0);
+    digitalWrite(PIN_LED3, 0);
+    digitalWrite(PIN_LED2, 1);
     delay(INTERVAL);
     digitalWrite(PIN_LED2, 0);
+    digitalWrite(PIN_LED1, 1);
     delay(INTERVAL);
-    digitalWrite(PIN_LED3, 0);
-    delay(INTERVAL);
+    digitalWrite(PIN_LED1, 0);
 }
